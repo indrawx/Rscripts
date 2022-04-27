239c407a84a6aeb3d6e6be69a3f9f805b8fceeac
indrawx <93877980+indrawx@users.noreply.github.com>
2022-04-27 22:02:37 +0200

    Update Eratosthen.R

--- Eratosthen.R
+++ Eratosthen.R
@@ -1,33 +1,29 @@
-# "Run" a poté zavolat fci v konzoli jako: Eratosthen('01_data.csv') 
-# x je vstupem funkce, naètený z csv souboru 01_data.csv
-# výstupem je zápis seøazených prvoèísel nalezených v 01_data.csv do nového souboru vystupni_soubor.csv
-
 Eratosthen <- function(data){
-  x <- read.table(data, header = F)$V1 # naètení dat do promìnné x
-  x <- sort(x) # seøazení èísel od nejmenšího po nejvìtší
-  x <- x[-1] # odstranìní 1 z vektoru x (není prvoèíslo)
+  x <- read.table(data, header = F)$V1 
+  x <- sort(x) 
+  x <- x[-1]
   o <- 1 
-  n <- length(x) # n je délka vektoru x
-  m <- length(x) # m je délka vektoru x
+  n <- length(x) 
+  m <- length(x) 
   
-  while (o <= sqrt(n)){ # cyklus probíhá až k odmocninì z délky vstupního vektoru
-    f <- 2*x[o] # f je hledaná pozice
-    p <- o+1 # p je pozice, ke které je pøièten poèáteèní index o, navyšován v dalších cyklech o 1
+  while (o <= sqrt(n)){ 
+    f <- 2*x[o] 
+    p <- o+1 
     
     while (p <= m){
-      if (x[p] == f){ # pokud pozice èísla na vstupu odpovídá hledané...
-        f <- f + x[o] # k vektoru hledaných pozic se pøipíše indexace vstupního vektoru
-        x <- x[-1*p] # pøepis vstupního vektoru na výstupní
-        m <- length(x)} # délka vst. vektoru
+      if (x[p] == f){
+        f <- f + x[o] 
+        x <- x[-1*p] 
+        m <- length(x)} 
       else if (x[p] > f){ 
-        f <- f + x[o] # skok na další možnou hledanou pozici
+        f <- f + x[o] 
       }
       else { 
-        p <- p + 1 # ochrana
+        p <- p + 1 
       }
     }
-    o <- o + 1 # navýšení indexu o
+    o <- o + 1 
   }
-  write.csv(x, file = 'vystupni_soubor.csv', row.names=FALSE, na="") # zapsání dat do nového souboru
+  write.csv(x, file = 'vystupni_soubor.csv', row.names=FALSE, na="") 
   }
 
